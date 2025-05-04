2d282eee32cbcdd4b10ca6346e31940e393b73e6
xzyacbprq <anonymous.icml21@gmail.com>
2025-05-04 15:09:02 +0900

    saving update

--- libs/torchgeo/tests/datasets/azcopy.bat
+++
@@ -1,6 +0,0 @@
-REM Copyright (c) Microsoft Corporation. All rights reserved.
-REM Licensed under the MIT License.
-
-@ECHO OFF
-
-python3 tests\datasets\azcopy.py %*
--- libs/torchgeo/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-import subprocess
-import sys
-
-
-def test_help() -> None:
-    subprocess.run([sys.executable, '-m', 'torchgeo', '--help'], check=True)
